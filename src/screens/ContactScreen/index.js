34c5f71198090c034fd2670bf3ddd5a937a8c6d7
Koushik Mondal <darkdeavil004@gmail.com>
2023-09-04 19:42:36 +0530

    Search filter added

--- src/screens/ContactScreen/index.js
+++ src/screens/ContactScreen/index.js
@@ -1,44 +1,58 @@
-import React, { useState } from "react";
-import { FlatList, StyleSheet, View,Text, TextInput } from "react-native";
-import contacts from '../../../assets/data/contacts.json'
+import React, {useState, useEffect} from 'react';
+import {FlatList, StyleSheet, View, Text, TextInput} from 'react-native';
+import dummyContacts from '../../../assets/data/contacts.json';
 
+const ContactScreen = () => {
+  const [searchInput, setSearchInput] = useState('');
+  // console.log(searchInput);
+  const [filteredContacts, setfilteredContacts] = useState(dummyContacts);
 
+  useEffect(() => {
+    const newContacts = dummyContacts.filter(contact =>
+      contact.user_display_name
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    );
+    setfilteredContacts(newContacts);
+  }, [searchInput]);
 
-const ContactScreen =()=>{
-    const [searchInput, setSearchInput] =useState('');
-    // console.log(searchInput);
-
-    return(
-        <View style={styles.TopView}>
-            <TextInput placeholder="search"  style={styles.SearchInput} onChangeText={setSearchInput} value={searchInput} />
-            <FlatList
-            data={contacts}
-            renderItem={({item})=>(<Text style={styles.ContactsName}>{item.user_display_name}</Text>)}
-            ItemSeparatorComponent={()=><View style={styles.seperator}/>}
-            />
-      </View>
-    )
-}
-
+  return (
+    <View style={styles.TopView}>
+      <TextInput
+        placeholder="search"
+        style={styles.SearchInput}
+        onChangeText={setSearchInput}
+        value={searchInput}
+      />
+      <FlatList
+        data={filteredContacts}
+        renderItem={({item}) => (
+          <Text style={styles.ContactsName}>{item.user_display_name}</Text>
+        )}
+        ItemSeparatorComponent={() => <View style={styles.seperator} />}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    TopView:{
-      padding:15,
-    },
-    ContactsName:{
-      fontSize:16,
-      marginVertical:10,
-    },
-    seperator:{
-      width:'100%',
-      height:1,
-      backgroundColor:'#dedede'
-    }, 
-    SearchInput:{
-        backgroundColor:'#f0f0f0',
-        padding:10,
-        borderRadius:5,
-    }
-  })
+  TopView: {
+    padding: 15,
+  },
+  ContactsName: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  seperator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#dedede',
+  },
+  SearchInput: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 
-  export default ContactScreen
+export default ContactScreen;
