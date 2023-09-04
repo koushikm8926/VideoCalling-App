3dabaf7cdc30af88da640f81caf0d7183426eaf4
Koushik Mondal <darkdeavil004@gmail.com>
2023-09-04 11:07:09 +0530

    search bar added

--- src/screens/ContactScreen/index.js
+++ src/screens/ContactScreen/index.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { FlatList, StyleSheet, View,Text } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View,Text, TextInput } from "react-native";
 import contacts from '../../../assets/data/contacts.json'
+
+
+
 const ContactScreen =()=>{
+    const [searchInput, setSearchInput] =useState('');
+    // console.log(searchInput);
+
     return(
         <View style={styles.TopView}>
-        <FlatList
-        data={contacts}
-        renderItem={({item})=>(<Text style={styles.ContactsName}>{item.user_display_name}</Text>)}
-        ItemSeparatorComponent={()=><View style={styles.seperator}/>}
-        />
+            <TextInput placeholder="search"  style={styles.SearchInput} onChangeText={setSearchInput} value={searchInput} />
+            <FlatList
+            data={contacts}
+            renderItem={({item})=>(<Text style={styles.ContactsName}>{item.user_display_name}</Text>)}
+            ItemSeparatorComponent={()=><View style={styles.seperator}/>}
+            />
       </View>
     )
 }
@@ -26,6 +33,11 @@ const styles = StyleSheet.create({
       width:'100%',
       height:1,
       backgroundColor:'#dedede'
+    }, 
+    SearchInput:{
+        backgroundColor:'#f0f0f0',
+        padding:10,
+        borderRadius:5,
     }
   })
 
