381c589b9f0afbc97d6c2ed46343818548891a1b
Koushik Mondal <darkdeavil004@gmail.com>
2023-09-04 20:00:43 +0530

    Calling screen created

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ContactScreen from './src/screens/ContactScreen'
-
+import CallingScreen from './src/screens/CallingScreen'
 
 const App = () => {
   return (
     <SafeAreaView >
       <StatusBar barStyle={'default'} />
-      <ContactScreen/>
+      <CallingScreen/>
     </SafeAreaView>
   )
 }
---
+++ src/screens/CallingScreen/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CallingScreen = () => {
+  return (
+    <View>
+      <Text>CallingScreen</Text>
+    </View>
+  )
+}
+
+export default CallingScreen
+
+const styles = StyleSheet.create({})
