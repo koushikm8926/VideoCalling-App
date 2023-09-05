d98c8ef59d7f3dc9efb9d6b8881fce7ec3f073e4
Koushik Mondal <darkdeavil004@gmail.com>
2023-09-05 18:57:25 +0530

    icons updated

--- src/screens/CallingScreen/index.js
+++ src/screens/CallingScreen/index.js
@@ -1,19 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
 const CallingScreen = () => {
   return (
     <View style={styles.page}>
+        
         <View style={styles.CameraPreview}>
             <Text style={styles.name}>Rahul</Text>
             <Text style={styles.phoneNumber}>+91 73 8481 0162</Text>
         </View>
 
         <View style={styles.buttonContainer}>
-           <Ionicons name="camera-reverse" color="white"  size={30}/>
+            <View style={styles.iconButton}>
+              <Ionicons name="camera-reverse" color="white"  size={30}/>
+            </View>
+            <View style={styles.iconButton}>
+              <MaterialIcons name="camera-off" color="white"  size={30}/>
+            </View>
+            <View style={styles.iconButton}>
+              <MaterialIcons name="microphone-off" color="white"  size={30}/>
+            </View>
+            <View style={[styles.iconButton, {backgroundColor:'red'}]}>
+              <MaterialIcons name="phone-hangup" color="white"  size={30}/>
+            </View>
         </View>
 
     </View>
@@ -24,6 +37,7 @@ const CallingScreen = () => {
 const styles = StyleSheet.create({
     page:{
         height:'100%',
+        backgroundColor:'grey'
     },
     CameraPreview:{
         flex:1,
@@ -45,10 +59,17 @@ const styles = StyleSheet.create({
     }, 
     buttonContainer:{
         backgroundColor:'#333333',
-        padding:10,
+        padding:20,
+        paddingBottom:40,
         borderTopRightRadius:15,
         borderTopLeftRadius:15,
-    
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    iconButton:{
+        backgroundColor:'#4a4a4a',
+        borderRadius:50,
+        padding:15,
     }
 
 })
